ec15762a3dcc6faeed9a400be70f60392f72e521
itsiprikshit <prikshittekta@gmail.com>
2024-04-23 17:23:20 -0600

    clicks script

--- clicks.py
+++ clicks.py
@@ -8,6 +8,18 @@ import math
 import subprocess
 import chardet
 
+seed_path = './out/.cur_input'
+
+if len(sys.argv) > 2 and sys.argv[1] != None and sys.argv[2] != None:
+    target_program = sys.argv[1]
+    seed_path = sys.argv[2]
+
+    def task():
+        os.system(target_program)
+
+    process = Process(target=task)
+    process.start()
+
 def sfc32(a, b, c, d):
     a &= 0xFFFFFFFF
     b &= 0xFFFFFFFF
@@ -44,46 +56,61 @@ def generateRandomNumber(seedstring):
     num = xfnv1a(seedstring)
     return sfc32(num(), num(), num(), num())
 
+def getWindowCoords():
+    window_list = [] #start x, start y, width, height
+    window_command = "xwininfo -id $(xdotool getactivewindow)"
+    window_info = subprocess.check_output(window_command, shell=True)
+    window_info = window_info.decode('utf-8')
+    info = window_info.split("\n")
+    for res in info:
+        temp = res.split(":")
+        if(len(temp)>1):
+            match temp[0].strip():
+                case "Absolute upper-left X":
+                    window_list.append(int(temp[1].strip()))
+                case "Absolute upper-left Y":
+                    window_list.append(int(temp[1].strip()))
+                case "Width":
+                    window_list.append(int(temp[1].strip()))
+                case "Height":
+                    window_list.append(int(temp[1].strip()))
+    return window_list
+
 def getScreenResolution():
     resolution_command = "xdpyinfo | grep dimensions | awk '{ print $2 }'"
     resol_value = subprocess.check_output(resolution_command, shell=True)
     resol_value = resol_value.decode('utf-8')
     resol_value_split = resol_value.split('x')
-    print("The output is : "+resol_value_split[0])
     return int(resol_value_split[0])
 
+time.sleep(3)
+
+window_coords = getWindowCoords()
+start_x = window_coords[0]
+start_y = window_coords[1]
+width = window_coords[2]
+height = window_coords[3]
+
+y_padding = 40
+
 seed = ""
-with open('./out/.cur_input', 'r', encoding='iso-8859-1') as f:
+
+with open(seed_path, 'r', encoding='iso-8859-1') as f:
 	seed = f.read()
+
+# Add a sleep when running in Qemu mode because the target program takes a lot of time to open
+# time.sleep(25)
 
 random_number = generateRandomNumber(seed)
 
-for i in range(10):
-	val = random_number()
-	#print(val)
-	x_val = 63 + 390 * val
-	y_val = 360 + 230 * val
-	pyautogui.click(math.floor(x_val),math.floor(y_val))
+for i in range(20):
+    val = random_number()
+    x_val = (start_x - 10) + width * val
+    y_val = (start_y + y_padding - 10) + (height - y_padding) * val
+    pyautogui.click(math.floor(x_val),math.floor(y_val))
 
-pyautogui.click(386,100)
+time.sleep(1)
 
-#full window
-#x_val = 51 + 412 * random_number
-# #y_val = 78 + 541 * random_number
-# window_coords = getWindowCoords()
-# resolution = getScreenResolution()
-#x_val = 63 + 390 * random_number
-#y_val = 360 + 230 * random_number
-# start_x = window_coords[0]
-# start_y = window_coords[1]
-# width = window_coords[2]
-# height = window_coords[3]
-# x_val = start_x-10 + width*random_number
-# y_val = start_y-10 + height*random_number
-#time.sleep(2)
-# print(x_val)
-# print(y_val)
-# pyautogui.click(math.floor(x_val),math.floor(y_val))
-# time.sleep(2)
-#pyautogui.click(386,100)
-
+pyautogui.keyDown('ctrlleft')
+pyautogui.press('q')
+pyautogui.keyUp('ctrlleft')
